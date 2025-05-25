d3a2efb1022461c2215d45f89aa76863baf06198
Josuel Beserra Mroczko <josuelbeserramroczko@gmail.com>
2025-05-25 07:29:15 -0300

    5
    5

--- components/videos/detalhes.vue
+++ components/videos/detalhes.vue
@@ -1,13 +1,20 @@
+<script setup lang="ts">
+const {diasPessados , diferencaEmDias}= useFormatacao()
+debugger
+const dadaDoDia = new Date('2023, 9, 2')
+
+diasPessados(dadaDoDia)
+</script>
+
 <template>
     <div>
-        <h1>Componentes - nuxt 3</h1>
-        <p>Autor: Josuel</p>
+            <h1>Componentes - nuxt3</h1>
+            <p>autor: josuel</p>
+            <p>{{diferencaEmDias}}dias atraz</p>
     </div>
 </template>
 
-<script setup>
 
-</script>
 
 <style lang="scss" scoped>
 
